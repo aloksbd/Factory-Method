1d361b01e3ec8c1deacb100212720306e9348943
alok <alksbd9@gmail.com>
2021-08-08 23:32:26 +0545

    Auto layout EmployeesView in EmployeesViewController

--- Factory/Factory/UI/Controllers/EmployeesViewController.swift
+++ Factory/Factory/UI/Controllers/EmployeesViewController.swift
@@ -9,19 +9,30 @@ import UIKit
 
 final class EmployeesViewController: UIViewController {
     private var presenter: EmployeesPresenter!
-    private var employeesView: EmployeesView!
+    private var employeesView: UIView!
     
     convenience init(presenter: EmployeesPresenter, employeesView: EmployeesView) {
         self.init()
         self.presenter = presenter
-        self.employeesView = employeesView
+        self.employeesView = employeesView.getView()
+        self.employeesView.translatesAutoresizingMaskIntoConstraints = false
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         presenter.loadEmployees()
-        view.addSubview(employeesView.getView())
+        view.addSubview(employeesView)
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            employeesView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            employeesView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            employeesView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            employeesView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
     }
 
 }
