15a8717a0eda69e3f1a64a3b0fc543d90494e926
alok <alksbd9@gmail.com>
2021-08-13 15:47:42 +0545

    Remote image loader

--- Factory/Factory/Core/Repository/Employees Api/HTTPClient.swift
+++ Factory/Factory/Core/Repository/Employees Api/HTTPClient.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
- protocol HTTPClient {
+protocol HTTPClient {
     typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
     
     func get(from url: URL, completion: @escaping (Result) -> Void)
---
+++ Factory/Factory/Core/Repository/Employees Api/URLSession/RemoteEmployeeImageDataLoader.swift
@@ -0,0 +1,44 @@
+//
+//  RemoteEmployeeImageDataLoader.swift
+//  Factory
+//
+//  Created by alok subedi on 13/08/2021.
+//
+
+import Foundation
+
+public protocol EmployeeImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void)
+}
+
+final class RemoteEmployeeImageDataLoader: EmployeeImageDataLoader {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (EmployeeImageDataLoader.Result) -> Void){
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result{
+            case .failure:
+                completion(.failure(Error.connectivity))
+            case let .success((data, response)):
+                if response.statusCode == 200 && !data.isEmpty {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(Error.invalidData))
+                }
+            }
+        }
+    }
+}
